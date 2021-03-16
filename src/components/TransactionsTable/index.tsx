4552a96ddb70e4bb98bcd13d3dc2778abd886829
Silvio-Hub <silvioand1@hotmail.com>
2021-03-16 09:29:58 -0300

    Componentização, consumindo API

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+//consumindo uma API ficticia
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, []);
+
+  //Minha tabela
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/12/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.300</td>
+            <td>Casa</td>
+            <td>13/12/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
